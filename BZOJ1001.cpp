f4df98f893454cb75e7340241b485c06d6fda5f4
FirstLast <fstqwq@foxmail.com>
2016-12-13 15:11:22 +0800

    Add files via upload

--- BZOJ1001.cpp
+++ BZOJ1001.cpp
@@ -1,118 +1,195 @@
-/*This is a source code from FirstLast*/
 #include <iostream>
 #include <cstdio>
 #include <cstring>
 #include <cmath>
-#include <ctime>
-#include <cstdlib>
 #include <algorithm>
 #include <queue>
-#include <string>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
+#define MAXN 1005
+#define s 2097152
+#define INF 0x1f1f1f1f
+#define MAX_BUF_SIZE 10485765
 
-#define s 65536
+int ch=0;
 
-typedef unsigned int ui;
-
-ui n,t[s<<1],full[s<<1],sum[s<<1],lazy[s<<1];
-int L[s<<1],R[s<<1],M[s<<1];
-int ch;
-inline void read(ui &a) {
-	while (ch<'0' || ch>'9') ch=getchar();
-	a=0;
-	while (ch>='0' && ch<='9') ch=getchar();
+inline int readInt(void) {
+    while (ch < '0' || ch > '9') ch=getchar();
+    int r=0;
+    while (ch>='0' && ch<='9') {r = r * 10 + ch - '0';ch=getchar();}
+    return r;
 }
 
-inline void init() {
-	sum[0]=0;
-	for (ui i=s,j=1;j<=s;j++) {
-		L[i]=R[i]=M[i]=j;
-		full[i]=j;
-		sum[j]=sum[j-1]+j;
-	}
-	for (ui i=s-1;i;i--) {
-		full[i]=full[i<<1]+full[(i<<1)|1];
-		L[i]=L[i<<1];
-		M[i]=R[i<<1];
-		R[i]=R[(i<<1)|1];
-	}
+/* Main */
+struct edge {
+	int next,to,w;
+}e[8000005];
+int n,m,ecnt=0,src,sink,nm;
+int dis[2*MAXN*MAXN],head[2*MAXN*MAXN],t[s<<1];
+int row[MAXN][MAXN],col[MAXN][MAXN],slp[MAXN][MAXN],tag[MAXN][MAXN][2];
+bool visit[2*MAXN*MAXN];
+inline void add(int u,int v,int w) {
+//	printf("%d->%d %d\n",u,v,w);
+	e[++ecnt].to=v;
+	e[ecnt].next=head[u];
+	e[ecnt].w=w;
+	head[u]=ecnt;
+	e[++ecnt].to=u;
+	e[ecnt].next=head[v];
+	e[ecnt].w=w;
+	head[v]=ecnt;
 }
 
-inline void pushdown(int x) {
-	if (x==1) {
-		t[x<<1]=t[(x<<1)|1]=full[x<<1];
+/*inline void build() {
+	nm=(n-1)*(m-1);
+	src=0,sink=2*nm+1;
+//	puts("A");
+	for (int i=nm+1,lab=1;i<sink;i+=m-1,lab++) {
+		add(src,i,col[lab][1]);
 	}
-	else {
-		t[x<<1]=t[(x<<1)|1]=0;
+//	puts("B");
+	for (int i=sink-m+1,lab=1;i<sink;i++,lab++) {
+		add(src,i,row[n][lab]);
 	}
-	lazy[x<<1]=lazy[(x<<1)|1]=lazy[x];
-}
-
-void fill(int x,int l,int r) {
-	if (L[x]==l && R[x]==r) {
-		t[x]=full[x];
-		lazy[x]=1;
-		return;
+//	puts("C");
+	for (int i=1;i<m;i++) {
+		add(i,sink,row[1][i]);
 	}
-	if (lazy[x]) pushdown(x);
-	lazy[x]=0;
-	int mid=M[x];
-	if (l<=mid) fill(x<<1,l,min(mid,r));
-	if (r>mid) fill((x<<1)|1,max(mid+1,l),r);
-	t[x]=t[x<<1]+t[(x<<1)|1];
-}
-
-void clear(int x,int l,int r) {
-	if (L[x]==l && R[x]==r) {
-		t[x]=0;
-		lazy[x]=-1;
-		return;
+//	puts("D");
+	for (int i=n,lab=1;i<sink-nm;i+=m-1,lab++) {
+		add(i,sink,col[lab][m]);
 	}
-	if (lazy[x]) pushdown(x);
-	lazy[x]=0;
-	int mid=M[x];
-	if (l<=mid) clear(x<<1,l,min(mid,r));
-	if (r>mid) clear((x<<1)|1,max(mid+1,l),r);
-	t[x]=t[x<<1]+t[(x<<1)|1];
-}
-
-ui query(int x,int l,int r) {
-	if (lazy[x]) {
-		if (x==1) {
-			return sum[r]-sum[l-1];
-		}
-		else {
-			return 0;
+//	puts("E");
+	// row
+	for (int i=2;i<n;i++) {
+		int from=nm+(i-2)*(m-1)+1,to=(i-1)*(m-1)+1;
+		for (int j=1;j<m;j++,from++,to++) {
+			add(from,to,row[i][j]);
 		}
 	}
-	if (L[x]==l && R[x]==r) {
-		return t[x];
+//	puts("F");
+	// col
+	for (int j=2;j<m;j++) {
+		int from=j-1,to=j+nm;
+		for (int i=1;i<n;i++,from+=m-1,to+=m-1) {
+			add(from,to,row[i][j]);
+		}
 	}
-	if (lazy[x]) pushdown(x);
-	int mid=M[x];
-	ui ret=0;
-	if (l<=mid) ret=query(x<<1,l,min(mid,r));
-	if (r>mid) ret=ret+query((x<<1)|1,max(mid+1,l),r);
-	return ret;
+//	puts("G");
+	// slope
+	for (int i=1;i<n;i++) {
+		int tag=1+(i-1)*(m-1);
+		for (int j=1;j<m;j++,tag++) {
+			add(tag,tag+nm,slp[i][j]);
+		}
+	}
+//	puts("H");
+}*/
+
+void build2() {
+	src=0,sink=1;
+	int cnt=1;
+	for (int i=1;i<n;i++) {
+		for (int j=1;j<m;j++) {
+			tag[i][j][0]=++cnt;
+			tag[i][j][1]=++cnt;
+		}
+	}
+	for (int i=1;i<m;i++) {
+		add(tag[1][i][0],sink,row[1][i]);
+	}
+	for (int i=1;i<n;i++) {
+		add(tag[i][m-1][0],sink,col[i][m]);
+	}
+	for (int i=1;i<n;i++) {
+		add(tag[i][1][1],src,col[i][1]);
+	}
+	for (int i=1;i<m;i++) {
+		add(tag[n-1][i][1],src,row[n][i]);
+	}
+	for (int i=2;i<n;i++)
+		for (int j=1;j<m;j++) {
+			add(tag[i-1][j][1],tag[i][j][0],row[i][j]);
+		}
+	for (int j=2;j<m;j++) {
+		for (int i=1;i<n;i++) {
+			add(tag[i][j-1][0],tag[i][j][1],col[i][j]);
+		}
+	}
+	for (int i=1;i<n;i++)
+		for (int j=1;j<m;j++) {
+			add(tag[i][j][0],tag[i][j][1],slp[i][j]);
+		}
+}
+
+inline void update(int x,int d) {
+    t[x+=s]=d;
+    for (x>>=1;x;x>>=1) t[x]=min(t[x<<1],t[(x<<1)|1]);
+}
+
+inline int query() {
+    int x=1;
+    while (x<s) {
+        if (t[x<<1]<t[(x<<1)|1])
+            x<<=1;
+        else
+            x=(x<<1)|1;
+    }
+    return x-s;
+}
+  
+
+inline void dijkstra() {
+	memset(dis,31,sizeof(dis));
+	memset(t,31,sizeof(t));
+	update(src,0);
+	int x=src;
+	while (t[x+s]<INF) {
+		dis[x]=t[x+s];
+		update(x,INF);
+		visit[x]=1;
+		if (x==sink) return;
+		for (int now=head[x],v;now;now=e[now].next) {
+            v=e[now].to;
+            if (!visit[v] && dis[x]+e[now].w<t[s+v]) {
+                update(v,dis[x]+e[now].w);
+            }
+        }
+        x=query();
+	}
 }
 
 int main() {
-	init();
-	read(n);
-	ui op,l,r;
-	for (ui i=1;i<=n;i++) {
-		read(op);read(l);read(r);
-		if (op) {
-			if (op==1) {
-				fill(1,l,r);
-			}
-			else {
-				clear(1,l,r);
-			}
+	n=readInt();
+	m=readInt();
+	if(n==1 || m==1) {
+		if(n>m) swap(n, m);
+		int ans=INF;
+		for(int i=1, a; i<m; i++) {
+			a=readInt();
+			if(ans>a) ans=a;
 		}
-		else {
-			printf("%u\n",query(1,l,r));
-		}
+		printf("%d\n", ans==INF?0:ans);
+		return 0;
 	}
+	for (int i=1;i<=n;i++)
+		for (int j=1;j<m;j++) {
+			row[i][j]=readInt();
+		}
+	for (int i=1;i<n;i++)
+		for (int j=1;j<=m;j++) {
+			col[i][j]=readInt();
+		}
+	for (int i=1;i<n;i++)
+		for (int j=1;j<m;j++) {
+			slp[i][j]=readInt();
+		}
+	build2();
+	dijkstra();
+	printf("%d\n",dis[sink]);
 	return 0;
 }
+
+
+
